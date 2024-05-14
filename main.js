22a4cd67d796caf3c22c2c02c2a6d0d2cfb47b2c
unknown <adrianforero@colegioguanenta.edu.co>
2024-05-14 12:40:14 -0500

    se añadió una scroll animation

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+// Scroll animations
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry =>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
